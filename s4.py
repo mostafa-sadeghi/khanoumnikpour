fb4c971b892a6dc5d47548cbb63e6a5d29f4bbdb
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-13 20:59:28 +0330

    sadeghi

---
+++ s4.py
@@ -0,0 +1,97 @@
+# i = 0
+# while i < 10:
+#     print("Hello")
+#     print("some thing else")
+#     i = i + 1 # i += 1
+
+# print("outside")
+
+# for i in range(10):
+#     print("Hello")
+#     print("some thing else")
+
+# for i in [1,4,7,0,-1]:
+#     print(i**2)
+
+# for s in 'python':
+#     print(s.upper(), end=" ")
+
+course = 'python'
+
+# for i in range(len(course)):
+#     print(course[i].upper(), end=" ")
+
+
+# for index,value in enumerate(course):
+#     # print(item[0],item[1])
+#     print(index, value)
+
+# matrix = [
+#     [0, 1, 2, 3],
+#     [10, 11, 12, 13],
+# ]
+
+# # print(matrix[0][0])
+# for row in matrix:
+#     for col in row:
+#         print(f'{col:^3}', end=' ')
+#     print()
+
+# numbers = [1, 'ali', [1, 2, 3]]
+# print(numbers[0])
+# print(numbers[1])
+
+# numbers.append(100)
+
+# print(numbers)
+# user_input = int(input('enter a number: '))
+# numbers.remove(user_input)
+
+# print(numbers)
+# del numbers[1]
+# print(numbers)
+
+numbers = [1, 2, 3, 4, 5, 2, 2, 4]
+# number_of_two = numbers.count(2)
+# print(f'there are {number_of_two} two')
+# numbers.append(120)
+# print(numbers)
+# numbers.extend(['ali','tina','sara'])
+# print(numbers)
+
+# list1 = [1,2,3,4]
+# list2 = [5,6,7,8]
+
+# output = list1 + list2
+# print(output)
+# print(numbers.index(4))
+# numbers.insert(2, 300)
+# print(numbers)
+
+# removed_number = numbers.pop()
+# print("removed number:", removed_number)
+# print(numbers)
+
+numbers = [1, 2, 3, 4, 5, 2, 2, 4]
+
+# removed_number = numbers.pop(2)
+# print("removed number:", removed_number)
+# print(numbers)
+
+# numbers.reverse()
+# print(numbers)
+# numbers.sort(reverse=True)
+# print(numbers)
+# new_list = sorted(numbers)
+# print("numbers:",numbers)
+# print("new_list:",new_list)
+
+# numbers = (1, 2, 6, 7, 8)
+# print(type(numbers))
+# numbers[0] = 100
+
+product = {
+    'id': 1,
+    'name': "artin",
+    'age': 32
+}
